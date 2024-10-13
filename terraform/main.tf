4c1107555bfbd89e1901687ca9da999c65b0f5d5
22algodaddy <choubeydhruv2014@gmail.com>
2024-10-13 19:54:17 +0530

    Created a simple react-app and send its artfiact to s3

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,5 +8,17 @@ terraform {
 }
 
 provider "aws" {
-  # Configuration options
+  region = "us-west-2"
+ }
+
+resource "aws_s3_bucket" "example" {
+  bucket = "hu-devops24-dhruv"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+output "bucket_name" {
+  value = aws_s3_bucket.example.id
 }
